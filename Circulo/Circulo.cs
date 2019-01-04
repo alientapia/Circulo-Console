7621e8040136d089d02fa0370e7d88e6a129cbb9
Alien Tapia Salvador <alientapia33@gmail.com>
2019-01-04 17:55:24 -0300

    Add project files.

---
+++ Circulo/Circulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circulo
+{
+    class Circulo
+    {
+        private double radio;
+
+        public double Radio { get => radio; set => radio = value; }
+        public Circulo()
+        {
+            radio = 0;
+        }
+        public Circulo(double r)
+        {
+            radio = r;
+        }
+        public double Perimetro()
+        {
+
+            double pi, perimetro;
+            pi = Convert.ToDouble(3.1416);
+            perimetro = 2 * this.radio * pi;
+            return perimetro;
+        }
+    }
+}
---
+++ Circulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            Console.WriteLine("Hello entre el radio del circulo: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            Circulo circulo = new Circulo(radio);
+            Console.WriteLine("El perimetro es:  "+ circulo.Perimetro());
+            Console.ReadLine();
+        }
+    }
+}
